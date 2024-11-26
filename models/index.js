3577657e9f76efae4f3b31717ec7b6cb70ae76e5
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2024-11-26 17:53:41 +0100

    11

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.ENV
+const env = 'production'
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
